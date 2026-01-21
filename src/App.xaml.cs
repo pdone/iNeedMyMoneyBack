28e2ee9f9af4153305023964e7d2428d477f3d30
pdone <617941447@qq.com>
2026-01-21 09:36:00 +0800

    增加单例模式

--- src/App.xaml.cs
+++ src/App.xaml.cs
@@ -2,6 +2,7 @@
 using System.Diagnostics;
 using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Threading;
 using System.Windows;
 using iNeedMyMoneyBack;
 
@@ -34,6 +35,66 @@ public partial class App : Application
     public const string ProductVersion = "1.4";
     public const string Copyright = "Copyright © pdone 2025";
 
+    #region 检查实例是否已存在 存在则将其显示到前台
+    private static Mutex _mutex;
+    private static bool _createNew;
+
+    [DllImport("user32.dll")]
+    private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+    [DllImport("user32.dll")]
+    private static extern bool IsIconic(IntPtr hWnd);
+
+    [DllImport("user32.dll")]
+    private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+    private bool InstanceExists()
+    {
+        _mutex = new Mutex(true, $"{ProductName}-{Copyright}", out _createNew);
+        if (!_createNew)
+        {
+            Logger.Info("Instance already exists");
+            BringExistingInstanceToFront();
+            return true;
+        }
+        return false;
+    }
+
+    private void BringExistingInstanceToFront()
+    {
+        // 获取当前应用程序的进程名
+        var processName = Process.GetCurrentProcess().ProcessName;
+
+        // 查找所有同名进程
+        var processes = Process.GetProcessesByName(processName);
+
+        foreach (var process in processes)
+        {
+            // 跳过当前进程
+            if (process.Id == Process.GetCurrentProcess().Id)
+            {
+                continue;
+            }
+
+            // 获取已存在实例的窗口句柄
+            var hWnd = process.MainWindowHandle;
+
+            if (hWnd != IntPtr.Zero)
+            {
+                // 如果窗口是最小化状态，先恢复窗口
+                if (IsIconic(hWnd))
+                {
+                    ShowWindow(hWnd, 9); // SW_RESTORE
+                }
+
+                // 将窗口显示到前台
+                SetForegroundWindow(hWnd);
+                break;
+            }
+        }
+    }
+    #endregion
+
     protected override void OnExit(ExitEventArgs e)
     {
         base.OnExit(e);
@@ -43,6 +104,12 @@ public partial class App : Application
     {
         base.OnStartup(e);
         AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+
+        if (InstanceExists())
+        {
+            Current.Shutdown();
+            return;
+        }
     }
 
     private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
